642493be31081e2bab0a4f3e07c5793f1c8f4c9e
Bharti025 <110337668+Bharti025@users.noreply.github.com>
2024-05-13 11:54:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,21 @@ const app=express();
 dotenv.config({path:"./config/config.env"} );
 // app.use(cors());
 
-app.use(cors(
-{
-  origin:[process.env.FRONTEND_URL],
-  methods:["POST"],
-  credentials:true }));
+// app.use(cors(
+// {
+//   origin:[process.env.FRONTEND_URL],
+//   methods:["POST"],
+//   credentials:true }));
+
+app.use(cors({
+  origin: 'https://bharti12.netlify.app',
+   methods:["POST"],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.options('/api/v1/reservation/send', cors());
 app.use(`/api/v1/reservation`,reservationRouter);
 app.get("/", (req, res, next)=>{
   return res.status(200).json({
